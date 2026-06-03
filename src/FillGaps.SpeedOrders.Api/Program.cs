640378255732c5b3f26ecdf768710e243ca98182
Leandro Machado <leoantunes.fm@gmail.com>
2026-06-02 23:24:18 -0300

    feat: Implementacao dos Health Checks

--- src/FillGaps.SpeedOrders.Api/Program.cs
+++ src/FillGaps.SpeedOrders.Api/Program.cs
@@ -5,6 +5,8 @@ using FillGaps.SpeedOrders.Infrastructure.Data;
 using FillGaps.SpeedOrders.Infrastructure.Messaging;
 using FillGaps.SpeedOrders.Infrastructure.Queries;
 using FillGaps.SpeedOrders.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Text.Json;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +27,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHealthChecks()
+    .AddSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")!,
+        name: "sql-server",
+        tags: new[] { "ready" })
+    .AddKafka(
+        setup =>
+        {
+            setup.BootstrapServers = builder.Configuration["Kafka:BootstrapServers"];
+            setup.MessageTimeoutMs = 5000;
+        },
+        name: "kafka-broker",
+        tags: new[] { "ready" });
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -36,5 +52,32 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapHealthChecks("/health/live", new HealthCheckOptions
+{
+    Predicate = _ => false 
+});
+
+app.MapHealthChecks("/health/ready", new HealthCheckOptions
+{
+    Predicate = check => check.Tags.Contains("ready"),
+    
+    ResponseWriter = async (context, report) =>
+    {
+        context.Response.ContentType = "application/json";
+        var result = JsonSerializer.Serialize(new
+        {
+            status = report.Status.ToString(),
+            totalDuration = report.TotalDuration.ToString(),
+            dependencies = report.Entries.Select(e => new
+            {
+                dependency = e.Key,
+                status = e.Value.Status.ToString(),
+                responseTime = e.Value.Duration.ToString()
+            })
+        });
+        await context.Response.WriteAsync(result);
+    }
+});
 
 app.Run();
